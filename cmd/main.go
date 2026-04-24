41d1b30016caf36913a1e9b4ad3b5072237f4dc5
miracle <miraclechukwu2011@gmail.com>
2026-04-24 19:06:26 +0100

    updated main.go with initial implementation

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,57 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type Job struct {
+	ID      string
+	Payload string
+}
+
+var queue []Job
+
 func main() {
-	fmt.Println("Starting github.com/mcchukwu/jobq")
+	go Worker()
+
+	for i := range 10 {
+		Enqueue(Job{
+			ID:      fmt.Sprintf("%d", i),
+			Payload: "task",
+		})
+	}
+
+	select {}
+}
+
+// Task queue / Message broker
+func Enqueue(job Job) {
+	queue = append(queue, job)
+}
+
+func Dequeue() (Job, bool) {
+	if len(queue) == 0 {
+		return Job{}, false
+	}
+
+	job := queue[0]
+	queue = queue[1:]
+	return job, true
+}
+
+// Worker
+func Worker() {
+	for {
+		job, ok := Dequeue()
+		if !ok {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		Process(job)
+	}
+}
+
+func Process(job Job) {
+	fmt.Println("Processing:", job.Payload)
 }
